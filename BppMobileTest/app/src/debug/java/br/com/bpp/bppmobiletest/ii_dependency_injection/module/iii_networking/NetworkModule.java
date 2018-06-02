4f6a145ddc34038cf2fa52efd36a4b434c215936
Alexandre Bianchi <xandebianchi@gmail.com>
2018-06-01 21:38:41 -0300

    Add files via upload

---
+++ BppMobileTest/app/src/debug/java/br/com/bpp/bppmobiletest/ii_dependency_injection/module/iii_networking/NetworkModule.java
@@ -0,0 +1,27 @@
+package br.com.bpp.bppmobiletest.ii_dependency_injection.module.iii_networking;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import br.com.bpp.bppmobiletest.iii_utils.ConstantesGerais;
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+
+// Métodos colocados como Static pois o módulo é abstrato e tem instâncias de métodos com @Provides. Devem ser estáticos, nesse caso.
+@Module
+public class NetworkModule {
+
+    @Singleton
+    @Provides
+    Call.Factory provideOkHttp() {
+        return new OkHttpClient.Builder().build();
+    }
+
+    @Provides
+    @Named("BaseUrl")
+    String provideBaseUrl() { // Url de debug igual a url de produção, se for diferente alterar aqui
+        return ConstantesGerais.URL_BASE;
+    }
+}
